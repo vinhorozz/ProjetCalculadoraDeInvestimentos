3a57a5c21565764e6d38c3790db90bd1047584f5
Flávio Queiroz <vinhorozz@hotmail.com>
2025-08-28 22:50:35 -0300

    fix: função para remover cabeçalho e corpo da tabela para realizar novo cálculo

--- src/table.js
+++ src/table.js
@@ -1,10 +1,12 @@
 const isNonEmptyArray=(arrayElement)=>{
     return Array.isArray(arrayElement) && arrayElement.length>0;
+
 }
 
 export const createTable=(columnsArray,dataArray, tableId)=>{
     if(!isNonEmptyArray(columnsArray)|| !isNonEmptyArray(dataArray) || !tableId){
         throw new Error("Para a correta execução, precisaremos de um array com as colunas, outro com as informações da linhas e também o 'id' do elemento 'tabela' selecionado");
+
     }
     const tableElement=document.getElementById(tableId);
  
@@ -14,13 +16,12 @@ export const createTable=(columnsArray,dataArray, tableId)=>{
     createTableHeader(tableElement,columnsArray);
     createTableBody(tableElement,dataArray,columnsArray);
 }
-
+// --------------------------------------------------------------------------------------------------------
 function createTableHeader(tableReference,columnsArray) { //Função deve ser ajustada para não criar cabeçalho dentro de tabela qua já está na página(limpar os dados)
-    const tableHeaderElement=tableReference.querySelector("thead")??(()=>{
-        const thead=document.createElement('thead');
-        tableReference.appendChild(thead);       
-        return thead;
-    })();        
+    const tableHeaderElement=tableReference.querySelector("thead");    
+    if(!tableHeaderElement){        
+        const tableHeaderElement=document.createElement('thead');
+        tableReference.appendChild(tableHeaderElement);
 
     const headerRow=document.createElement("tr");//Ao inserirmos nosvamente é preciso validar se já existe e abortar o processo
     ['bg-blue-900','text-slate-200','sticky','top-0'].forEach((cssClass)=>headerRow.classList.add(cssClass));
@@ -29,27 +30,36 @@ function createTableHeader(tableReference,columnsArray) { //Função deve ser aj
         const headerElement=/*html*/`<th class='text-center'>${tableColumnObject.columnLabel}</th>`
         headerRow.innerHTML+=headerElement;
     }
+
     tableHeaderElement.appendChild(headerRow);
+    }
 }
 // --------------------------------------------------------------------------------------------------------
 function createTableBody(tableReference,tableItems, columnsArray) { //Função deve ser ajustada para não criar um nova tabela dentro de tabela qua já está na página(limpar os dados)
-    const tableBodyElement=tableReference.querySelector("tbody")??(()=>{        
-        const tbody=document.createElement('tbody');
-        tableReference.appendChild(tbody);
-        return tbody;
-    })();        
+    let tableBodyElement=tableReference.querySelector("tbody");
+
+    if (!tableBodyElement) {
+        tableBodyElement=document.createElement('tbody');
+        tableReference.appendChild(tableBodyElement);                        
+    }
+    tableBodyElement.replaceChildren();
     
     for(const[itemIndex,tableItem] of tableItems.entries()){//criar as linhas da tabela        
         const tableRow=document.createElement('tr');        
 
-        if(itemIndex %2==0){tableRow.classList.add('bg-blue-200')}; //colorir alternadamente as linhas
+        if(itemIndex %2==0){
+           tableRow.classList.add('bg-blue-200')}; //colorir alternadamente as linhas
+
         for (const tableColumn of columnsArray) {
-            const formatFn=tableColumn.format ??(info=>info)//a função anônima repete o valor
+            const formatFn=tableColumn.format ??(info=>info)//a função anônima repete o valor            
             tableRow.innerHTML+=/*html*/`<td class="text-center">${formatFn(tableItem[tableColumn.accessor])}</td>` //célula da tabela//valor criado dinâmicamente
         }
-        tableReference.appendChild(tableRow);    
+        tableBodyElement.appendChild(tableRow);    
     }
 
-}
+    }
+
 // --------------------------------------------------------------------------------------------------------
- //Função deve ser ajustada para não criar  tabela dentra de tabela (limpar os dados
+function cleantables(){
+    
+}
